4bcf5d560717d444331f0a72dc70dc63294ed186
Davi_Dev8 <127767681+Davi8oliver@users.noreply.github.com>
2026-08-19 22:07:28 -0300

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,13 @@
+/*
+    Program to print "Hello world" as the output
+    Author: Davi Lopes
+    Data:08/19/2026
+*/
+
+public class hello {
+
+    public static void main (String[] args){
+        System.out.println("Hello World");
+    }
+
+}
